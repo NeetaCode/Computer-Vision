73e49bccd55c708870553f3dd671f0ce208f6c0d
Neeta <153780736+NeetaCode@users.noreply.github.com>
2025-09-02 15:05:30 -0600

    Create cifar10_vgg16_transfer_learning.py

---
+++ cifar10_vgg16_transfer_learning.py
@@ -0,0 +1,64 @@
+# VGG16 Model pretrained on Imagenet Weights for CIFAR-10
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
+
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+x = Flatten()(base_model.output)
+x = Dense(256, activation='relu')(x)
+x = Dropout(0.5)(x)
+output = Dense(10, activation='softmax')(x)
+
+model = Model(inputs=base_model.input, outputs=output)
+
+model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+
+checkpoint = ModelCheckpoint("vgg16_finetuned.h5", monitor="val_accuracy", save_best_only=True, verbose=1)
+early_stopping = EarlyStopping(monitor="val_loss", patience=10, verbose=1)
+
+batch_size = 32
+epochs = 30
+
+history = model.fit(
+    x_train, y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    validation_data=(x_test, y_test),
+    callbacks=[checkpoint, early_stopping],
+)
+
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
